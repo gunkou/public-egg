e8e6d71ba1195485350cd0a9969e3f52acf1f85e
gunkou <gun.kou-dev@outlook.com>
2020-03-25 00:35:34 +0900

    適当にヘッダーとフッターを作る

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,12 +1,17 @@
 <template>
   <footer class="site-footer">
-    <small  class="site-footer__copy">(c) 2020 public egg</small>
+    <small  class="site-footer__copy">(c) 2020 Public Egg</small>
   </footer>
 </template>
-<style>
-.site-footer__copy {
-  display: block;
-  font-style: normal;
-  text-align: center;
+
+<style lang="scss">
+.site-footer {
+  font-size: 1rem;
+  padding: 3rem 1rem;
+  &__copy {
+    display: block;
+    font-style: normal;
+    text-align: center;
+  }
 }
 </style>
