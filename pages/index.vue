c1f8eccc38ab74d1928a4a451cf1c7c17135ae41
gunkou <gun.kou-dev@outlook.com>
2020-03-27 22:24:10 +0900

    画像一覧の作成
    faviconの設定
    ハンバーガーメニューは非表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <ul>
-    <li v-for="item in items">
-      <img :src="`${item.imageSunnySideUp.url}?w=300`" alt="">
+  <ul class="media-list">
+    <li class="media-list__item"
+    v-for="item in items"
+    :key="item.id">
+      <picture class="media">
+        <source :srcset="`
+        ${item.imageSunnySideUp.url}?fm=webp&w=400,
+        ${item.imageSunnySideUp.url}?fm=webp&w=400&dpr=2 2x
+        `"
+        type="image/webp">
+        <img class="media__img" :src="`${item.imageSunnySideUp.url}?w=800`" alt="">
+      </picture>
     </li>
   </ul>
 </template>
@@ -30,18 +39,27 @@ export default {
 </script>
 
 <style lang="scss">
-ul {
+.media-list {
   margin: 0;
   padding: 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 400px);
+  justify-content: center;
+  grid-row-gap: 2rem;
+  grid-column-gap: 2rem;
+  &__item {
+    box-sizing: border-box;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
 }
-li {
-  margin: 5px;
-  padding: 0;
-  list-style: none;
-}
-img {
-  border: 1px solid #ccc;
-  vertical-align: middle;
-  max-width: 100%;
+.media {
+  &__img {
+    border: 1px solid #ccc;
+    vertical-align: middle;
+    max-width: 100%;
+    box-sizing: border-box;
+  }
 }
 </style>
